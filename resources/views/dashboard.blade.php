574ca07a8c2e8b0dc2a3d5a4480a208b0327abc9
Phanurat <paunavt5@gmail.com>
2023-11-10 11:50:18 +0700

    Style Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,46 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h5 class="font-semibold text-xl text-gray-800 leading-tight">
+            ยินดีต้อนรับสู่ {{ __('Dashboard') }}
+        </h5>
+        <h3>รายการทั้งหมด xxx ครั้ง</h3>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+    <br>  
+    <div class="container">
+        <div class="row">
+            <div class="input-group mb-3">
+                <input id="input1" class="form-control" type="text" placeholder="เงินสด" aria-label="Disabled input example" disabled>
+                <a class="btn btn-secondary" href="#">...</a>
+            </div>
+            
+            <div class="mb-3"></div> <!-- Add spacing -->
+    
+            <div class="input-group mb-3">
+                <input id="input2" class="form-control" type="text" placeholder="บัญชีธนาคาร" aria-label="Disabled input example" disabled>
+                <a class="btn btn-secondary" href="edit_page.html">...</a>
+            </div>
+            
+            <div class="mb-3"></div> <!-- Add spacing -->
+    
+            <div class="input-group mb-3">
+                <input id="input3" class="form-control" type="text" placeholder="ค้างรับ" aria-label="Disabled input example" disabled>
+                <a class="btn btn-secondary" href="edit_page.html">...</a>
+            </div>
+            
+            <div class="mb-3"></div> <!-- Add spacing -->
+    
+            <div class="input-group mb-3">
+                <input id="input4" class="form-control" type="text" placeholder="ค้างจ่าย" aria-label="Disabled input example" disabled>
+                <a class="btn btn-secondary" href="edit_page.html">...</a>
+            </div>
+            
+            <div class="mb-3"></div> <!-- Add spacing -->
+    
+            <div class="input-group">
+                <input id="input5" class="form-control" type="text" placeholder="คงเหลือ" aria-label="Disabled input example" disabled>
             </div>
         </div>
     </div>
+    
+    
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,10 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
         <!-- Styles -->
         @livewireStyles
     </head>
